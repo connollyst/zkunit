49619dbf2d484fb32dfeded87e207e7209f43031
Sean Connolly <connolly.st@gmail.com>
2015-07-26 14:39:06 +1000

    Replaced @RunWith with @Rule

--- src/main/java/org/zkoss/zkunit/ZKTest.java
+++ src/main/java/org/zkoss/zkunit/ZKTest.java
@@ -1,25 +1,25 @@
 package org.zkoss.zkunit;
 
-import static org.mockito.Matchers.anyBoolean;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.when;
-import static org.powermock.api.mockito.PowerMockito.mockStatic;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import org.junit.Before;
-import org.junit.runner.RunWith;
+import org.junit.Rule;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.rule.PowerMockRule;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.event.EventQueue;
 import org.zkoss.zk.ui.event.EventQueues;
 import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zk.ui.select.Selectors;
 import org.zkoss.zk.ui.util.Clients;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
 /**
  * <p>
@@ -33,37 +33,39 @@ import org.zkoss.zk.ui.util.Clients;
  *
  * @author Sean Connolly
  */
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({ Selectors.class, Clients.class, Events.class, EventQueues.class, Executions.class })
+@PrepareForTest({Selectors.class, Clients.class, Events.class, EventQueues.class, Executions.class})
 public abstract class ZKTest {
 
-	private final EventQueueLookupAnswer lookupAnswer = new EventQueueLookupAnswer();
-	private final Map<String, EventQueue> queues = new HashMap<>();
+    private final EventQueueLookupAnswer lookupAnswer = new EventQueueLookupAnswer();
+    private final Map<String, EventQueue> queues = new HashMap<>();
 
-	@Before
-	public void mockZKEnvironment() {
-		queues.clear();
-		mockStatic(Selectors.class);
-		mockStatic(Clients.class);
-		mockStatic(Events.class);
-		mockStatic(EventQueues.class);
-		mockStatic(Executions.class);
-		when(Events.isValid(anyString())).thenReturn(true);
-		when(EventQueues.lookup(anyString())).thenAnswer(lookupAnswer);
-		when(EventQueues.lookup(anyString(), anyBoolean())).thenAnswer(lookupAnswer);
-		when(EventQueues.lookup(anyString(), anyString(), anyBoolean())).thenAnswer(lookupAnswer);
-	}
+    @Rule
+    PowerMockRule rule = new PowerMockRule();
 
-	private class EventQueueLookupAnswer implements Answer<EventQueue> {
+    @Before
+    public void mockZKEnvironment() {
+        queues.clear();
+        mockStatic(Selectors.class);
+        mockStatic(Clients.class);
+        mockStatic(Events.class);
+        mockStatic(EventQueues.class);
+        mockStatic(Executions.class);
+        when(Events.isValid(anyString())).thenReturn(true);
+        when(EventQueues.lookup(anyString())).thenAnswer(lookupAnswer);
+        when(EventQueues.lookup(anyString(), anyBoolean())).thenAnswer(lookupAnswer);
+        when(EventQueues.lookup(anyString(), anyString(), anyBoolean())).thenAnswer(lookupAnswer);
+    }
 
-		@Override
-		public EventQueue answer(InvocationOnMock invocationOnMock) throws Throwable {
-			String name = invocationOnMock.getArguments()[0].toString();
-			if(!queues.containsKey(name)) {
-				queues.put(name, new SynchronousEventQueue());
-			}
-			return queues.get(name);
-		}
-	}
+    private class EventQueueLookupAnswer implements Answer<EventQueue> {
+
+        @Override
+        public EventQueue answer(InvocationOnMock invocationOnMock) throws Throwable {
+            String name = invocationOnMock.getArguments()[0].toString();
+            if (!queues.containsKey(name)) {
+                queues.put(name, new SynchronousEventQueue());
+            }
+            return queues.get(name);
+        }
+    }
 
 }
