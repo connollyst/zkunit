d692f00752f13f295bd15022349d49836c8384c5
Sean Connolly <connolly.st@gmail.com>
2017-12-14 15:55:45 +1000

    Event.send/echo/postEvent now immediately execute any listeners

--- src/main/java/org/zkoss/zkunit/ZKTest.java
+++ src/main/java/org/zkoss/zkunit/ZKTest.java
@@ -6,23 +6,22 @@ import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import org.zkoss.zk.ui.Executions;
-import org.zkoss.zk.ui.Session;
-import org.zkoss.zk.ui.Sessions;
-import org.zkoss.zk.ui.WebApp;
-import org.zkoss.zk.ui.event.EventQueue;
-import org.zkoss.zk.ui.event.EventQueues;
-import org.zkoss.zk.ui.event.Events;
+import org.zkoss.zk.ui.*;
+import org.zkoss.zk.ui.event.*;
 import org.zkoss.zk.ui.select.Selectors;
 import org.zkoss.zk.ui.util.Clients;
 import org.zkoss.zul.Filedownload;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.Assert.fail;
 import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.doAnswer;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.reflect.internal.WhiteboxImpl.findMethod;
 
 /**
  * <p>
@@ -40,6 +39,8 @@ import static org.powermock.api.mockito.PowerMockito.mockStatic;
 @PrepareForTest({Selectors.class, Sessions.class, Clients.class, Events.class, EventQueues.class, Executions.class, Filedownload.class})
 public abstract class ZKTest {
 
+    private static final Answer<Void> IMMEDIATE_ANSWER = new ImmediateEventAnswer();
+
     private final EventQueueExistsAnswer existsAnswer = new EventQueueExistsAnswer();
     private final EventQueueLookupAnswer lookupAnswer = new EventQueueLookupAnswer();
     private final EventQueueRemoveAnswer removeAnswer = new EventQueueRemoveAnswer();
@@ -47,8 +48,7 @@ public abstract class ZKTest {
     private final Map<String, EventQueue> queues = new HashMap<>();
 
     @Before
-    public void mockZKEnvironment() {
-        queues.clear();
+    public void mockZKEnvironment() throws Exception {
         mockStatic(Selectors.class);
         mockStatic(Sessions.class);
         mockStatic(Clients.class);
@@ -56,7 +56,12 @@ public abstract class ZKTest {
         mockStatic(EventQueues.class);
         mockStatic(Executions.class);
         mockStatic(Filedownload.class);
-        when(Events.isValid(anyString())).thenReturn(true);
+        mockEventQueues();
+        mockEvents();
+    }
+
+    private void mockEventQueues() {
+        queues.clear();
         when(EventQueues.exists(anyString())).thenAnswer(existsAnswer);
         when(EventQueues.lookup(anyString())).thenAnswer(lookupAnswer);
         when(EventQueues.lookup(anyString(), anyBoolean())).thenAnswer(lookupAnswer);
@@ -67,10 +72,32 @@ public abstract class ZKTest {
         when(EventQueues.remove(anyString(), any(Session.class))).thenAnswer(removeAnswer);
     }
 
+    private void mockEvents() throws Exception {
+        when(Events.isValid(anyString())).thenReturn(true);
+        handleEventImmediately("sendEvent", Event.class);
+        Events.sendEvent(any(Event.class));
+        handleEventImmediately("sendEvent", Component.class, Event.class);
+        Events.sendEvent(any(Component.class), any(Event.class));
+        handleEventImmediately("sendEvent", String.class, Component.class, Object.class);
+        Events.sendEvent(anyString(), any(Component.class), anyObject());
+        handleEventImmediately("echoEvent", Event.class);
+        Events.echoEvent(any(Event.class));
+        handleEventImmediately("echoEvent", String.class, Component.class, Object.class);
+        Events.echoEvent(anyString(), any(Component.class), anyObject());
+        handleEventImmediately("postEvent", Event.class);
+        Events.postEvent(any(Event.class));
+        handleEventImmediately("postEvent", String.class, Component.class, Object.class);
+        Events.postEvent(anyString(), any(Component.class), anyObject());
+    }
+
+    private void handleEventImmediately(String methodName, Class<?>... parameterTypes) throws Exception {
+        doAnswer(IMMEDIATE_ANSWER).when(Events.class, findMethod(Events.class, methodName, parameterTypes));
+    }
+
     private class EventQueueExistsAnswer implements Answer<Boolean> {
 
         @Override
-        public Boolean answer(InvocationOnMock invocationOnMock) throws Throwable {
+        public Boolean answer(InvocationOnMock invocationOnMock) {
             String name = invocationOnMock.getArguments()[0].toString();
             return queues.containsKey(name);
         }
@@ -78,7 +105,7 @@ public abstract class ZKTest {
 
     private class EventQueueLookupAnswer implements Answer<EventQueue> {
         @Override
-        public EventQueue answer(InvocationOnMock invocationOnMock) throws Throwable {
+        public EventQueue answer(InvocationOnMock invocationOnMock) {
             String name = invocationOnMock.getArguments()[0].toString();
             if (!queues.containsKey(name)) {
                 queues.put(name, new SynchronousEventQueue());
@@ -91,12 +118,69 @@ public abstract class ZKTest {
     private class EventQueueRemoveAnswer implements Answer<Boolean> {
 
         @Override
-        public Boolean answer(InvocationOnMock invocationOnMock) throws Throwable {
+        public Boolean answer(InvocationOnMock invocationOnMock) {
             String name = invocationOnMock.getArguments()[0].toString();
             if (queues.containsKey(name)) {
                 queues.remove(name);
             }
             return true;
         }
+    }
+
+    private static final class ImmediateEventAnswer implements Answer<Void> {
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public Void answer(InvocationOnMock invocation) {
+            Component eventTarget = getEventTarget(invocation.getArguments());
+            String eventName = getEventName(invocation.getArguments());
+            Object eventData = getEventData(invocation.getArguments());
+            if (eventTarget != null && eventTarget.getEventListeners(eventName) != null) {
+                for (EventListener listener : eventTarget.getEventListeners(eventName)) {
+                    try {
+                        listener.onEvent(new Event(eventName, eventTarget, eventData));
+                    } catch (Exception e) {
+                        fail(e.getMessage());
+                    }
+                }
+            }
+            return null;
+        }
+
+        private Component getEventTarget(Object... arguments) {
+            for (Object argument : arguments) {
+                if (argument instanceof Event) {
+                    return ((Event) argument).getTarget();
+                } else if (argument instanceof Component) {
+                    return (Component) argument;
+                }
+            }
+            throw new RuntimeException("Could not resolve event target from arguments: " + Arrays.toString(arguments));
+        }
+
+        private String getEventName(Object... arguments) {
+            for (Object argument : arguments) {
+                if (argument instanceof Event) {
+                    return ((Event) argument).getName();
+                } else if (argument instanceof String) {
+                    return (String) argument;
+                }
+            }
+            throw new RuntimeException("Could not resolve event target from arguments: " + Arrays.toString(arguments));
+        }
+
+        private Object getEventData(Object... arguments) {
+            // Reverse iterate to avoid returning the event name as the data
+            for (int i = arguments.length - 1; i >= 0; i--) {
+                Object argument = arguments[i];
+                if (argument instanceof Event) {
+                    return ((Event) argument).getData();
+                } else if (!(argument instanceof Component)) {
+                    return argument;
+                }
+            }
+            return null;
+        }
+
     }
 }
--- src/test/java/org/zkoss/zkunit/EventsTest.java
+++ src/test/java/org/zkoss/zkunit/EventsTest.java
@@ -2,9 +2,12 @@ package org.zkoss.zkunit;
 
 import org.junit.Test;
 import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
 import org.zkoss.zk.ui.event.Events;
+import org.zkoss.zul.Button;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
 /**
@@ -15,23 +18,122 @@ import static org.mockito.Mockito.mock;
 public class EventsTest extends ZKTest {
 
     @Test
-    public void shouldStubEventsEchoEvent() {
-        Events.echoEvent(mock(Event.class));
+    public void shouldAlwaysReportEventsAsValid() {
+        assertEquals(true, Events.isValid("hello world"));
     }
 
     @Test
-    public void shouldStubEventsPostEvent() {
-        Events.postEvent(mock(Event.class));
-    }
-
-    @Test
-    public void shouldStubEventsSendEvent() {
+    public void shouldNotHandleMockEventsInSend() {
         Events.sendEvent(mock(Event.class));
     }
 
     @Test
-    public void shouldStubClientsShowNotification() {
-        assertEquals(true, Events.isValid("hello world"));
+    public void shouldNotHandleMockEventsInEcho() {
+        Events.echoEvent(mock(Event.class));
+    }
+
+    @Test
+    public void shouldNotHandleMockEventsInPost() {
+        Events.postEvent(mock(Event.class));
+    }
+
+    @Test
+    public void shouldImmediatelyExecuteSendEvent1() {
+        // Given
+        Button button = new Button("Click me!");
+        TriggeredListener listener = new TriggeredListener();
+        button.addEventListener(Events.ON_CLICK, listener);
+        // When
+        Events.sendEvent(new Event(Events.ON_CLICK, button));
+        // Then
+        assertTrue(listener.isTriggered());
+    }
+
+    @Test
+    public void shouldImmediatelyExecuteSendEvent2() {
+        // Given
+        Button button = new Button("Click me!");
+        TriggeredListener listener = new TriggeredListener();
+        button.addEventListener(Events.ON_CLICK, listener);
+        // When
+        Events.sendEvent(button, new Event(Events.ON_CLICK, button));
+        // Then
+        assertTrue(listener.isTriggered());
+    }
+
+    @Test
+    public void shouldImmediatelyExecuteSendEvent3() {
+        // Given
+        Button button = new Button("Click me!");
+        TriggeredListener listener = new TriggeredListener();
+        button.addEventListener(Events.ON_CLICK, listener);
+        // When
+        Events.sendEvent(Events.ON_CLICK, button, "Hello world.");
+        // Then
+        assertTrue(listener.isTriggered());
+    }
+
+    @Test
+    public void shouldImmediatelyExecuteEchoEvent1() {
+        // Given
+        Button button = new Button("Click me!");
+        TriggeredListener listener = new TriggeredListener();
+        button.addEventListener(Events.ON_CLICK, listener);
+        // When
+        Events.echoEvent(new Event(Events.ON_CLICK, button));
+        // Then
+        assertTrue(listener.isTriggered());
+    }
+
+    @Test
+    public void shouldImmediatelyExecuteEchoEvent2() {
+        // Given
+        Button button = new Button("Click me!");
+        TriggeredListener listener = new TriggeredListener();
+        button.addEventListener(Events.ON_CLICK, listener);
+        // When
+        Events.echoEvent(Events.ON_CLICK, button, new Object());
+        // Then
+        assertTrue(listener.isTriggered());
+    }
+
+    @Test
+    public void shouldImmediatelyExecutePostEvent1() {
+        // Given
+        Button button = new Button("Click me!");
+        TriggeredListener listener = new TriggeredListener();
+        button.addEventListener(Events.ON_CLICK, listener);
+        // When
+        Events.postEvent(new Event(Events.ON_CLICK, button));
+        // Then
+        assertTrue(listener.isTriggered());
+    }
+
+    @Test
+    public void shouldImmediatelyExecutePostEvent2() {
+        // Given
+        Button button = new Button("Click me!");
+        TriggeredListener listener = new TriggeredListener();
+        button.addEventListener(Events.ON_CLICK, listener);
+        // When
+        Events.postEvent(Events.ON_CLICK, button, new Object());
+        // Then
+        assertTrue(listener.isTriggered());
+    }
+
+    private static final class TriggeredListener implements EventListener<Event> {
+
+        private boolean triggered = false;
+
+        @Override
+        public void onEvent(Event event) {
+            triggered = true;
+        }
+
+        boolean isTriggered() {
+            return triggered;
+        }
+
     }
 
 }
